f242cf811c8b1cf99257845e7526f69d39d6e386
ccmert <ccmert@outlook.com>
2016-12-01 15:40:40 +0300

    Creating the passenger class.

---
+++ src/Passenger.java
@@ -0,0 +1,71 @@
+
+public class Passenger {
+	
+	/*
+	 * 
+	 * Creating global variables for passenger.
+	 * 
+	 */
+	
+	private int tcNO;
+	private String name;
+	private String surname;
+	private String mail;
+	private String mobileTel;
+	private int baggageID;
+	
+	/*
+	 * 
+	 * Creating methods for the global variables
+	 * to get and set them.
+	 * 
+	 * Making those methods public to be able to edit them 
+	 * from other classes.
+	 * 
+	 */
+	
+	
+	public int getTcNO() {
+		return tcNO;
+	}
+	public void setTcNO(int tcNO) {
+		this.tcNO = tcNO;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getMobileTel() {
+		return mobileTel;
+	}
+	public void setMobileTel(String mobileTel) {
+		this.mobileTel = mobileTel;
+	}
+	public int getBaggageID() {
+		return baggageID;
+	}
+	public void setBaggageID(int baggageID) {
+		this.baggageID = baggageID;
+	}
+	
+	
+	
+	
+	
+	
+
+}
